dfec61410b4e1885b74cfe8792c9de260616fca1
Klavier <klaviergn@gmail.com>
2020-08-28 12:22:48 +0200

    Exercise 00.03 - Name input

---
+++ Exercise0003.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exercise0003 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Como te llamas?");
+		String nombre = sc.nextLine();
+		System.out.println("Hola " + nombre);
+	}
+}
