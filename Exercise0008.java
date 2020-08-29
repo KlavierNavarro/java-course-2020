c5c7c329f36029035c9167098601708d6ddfa40c
Klavier <klaviergn@gmail.com>
2020-08-29 22:08:34 +0200

    Exercise 00.08 - Asterisk square

--- Exercise0008.java
+++ Exercise0008.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 public class Exercise0008 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Introduce el lado del cuadrado:");
+		System.out.print("Introduce el lado del cuadrado: ");
 		int lado = sc.nextInt();
 		for(int i = 0;i < lado;i++){
 			for(int j = 0;j < lado;j++){
 				System.out.print("*");
-				}
+					}
 			System.out.println();
-			if(i == lado){
-				break;
 			}
 		}
 	}
-}
