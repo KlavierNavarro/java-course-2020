d4291b9e2395c2dfdeaace9125d5290dca37698b
Klavier <klaviergn@gmail.com>
2020-08-30 10:46:26 +0200

    Exercise 00.09 - Empty square asterisk

---
+++ Exercise0009.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exercise0009 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce el lado del cuadrado: ");
+		int lado = sc.nextInt();
+		for(int i = 0;i < lado;i++) {
+			System.out.print("*");
+			}
+		System.out.print("\r\n");
+		for(int i = 2;i < lado;i++) {
+			System.out.print("*");
+			if(i >= 1 && i < lado) {
+				for(int j = 1;j < lado-1;j++) {
+					System.out.print(" ");
+					}
+				System.out.print("*");
+				System.out.print("\r\n");
+				}
+			}
+		for(int i = 0;i < lado;i++) {
+			System.out.print("*");
+			}
+		}
+	}
